44d30a0435b801d5c453c6966c13dd41438832ad
Helen Korsmo <helen.korsmo@met.no>
2014-02-22 15:09:39 +0100

    Added function readVerifWdbCrosstab

--- readVerifWdb.R
+++ readVerifWdb.R
@@ -194,7 +194,6 @@ readVerifWdb<-function(wmo_no,period,model,prm,prg,lev=NULL,init.time=0,useRefti
 }
 
 
-
 readVerifWdbMultipleStations<-function(wmo_no,period,model,prm,prg,lev=NULL,init.time=0,useReftime=FALSE,verbose=TRUE){
 
   if (!started){
@@ -265,11 +264,6 @@ WHERE
         #convert values to other units
         values <- convertValues(values,par)
         results[parmod] <- values
-        levs <- results$lev
-        if (length(levs)!=0){
-          levs <- convertLevels(levs,par)
-          results$lev <- levs
-        }
       }
       
       if (nrow(allmodels)==0){      
@@ -291,9 +285,147 @@ WHERE
 }
 
 
+
+
+readVerifWdbCrosstab<-function(wmo_no,period,model,prm,prg,lev=NULL,init.time=0,useReftime=FALSE,verbose=TRUE){
+
+  if (!started){
+    cat("Please call startup(user, host, database) before using this function.\n")
+    return(FALSE)
+  }
+  
+  #create empty data frame to hold all models
+  allmodels<-data.frame()
+  
+  for (mod in model){
+    
+    sourcequery <- "SELECT (placename||'',''||validtimefrom||'',''||wci.prognosishour(referencetime, validtimefrom)) as rowid,
+        placename as WMO_NO, to_char(validtimeto,''YYYYMMDDHH24'') as TIME, wci.prognosishour(referencetime, validtimeto) AS PROG, valueparametername, value QUERY order by rowid,valueparametername"      
+    catquery <- "select distinct valueparametername QUERY order by valueparametername"    
+    query <- "
+FROM
+        vega.floatvalue,
+        vega.dataprovidergroups g
+WHERE
+        validtimeto-referencetime in (PROGSTRING) AND
+        extract(hour from referencetime) in (0) AND
+        placename in (WMOSTRING) AND
+        valueparametername in (PARAMETERSTRING) AND
+        referencetime >= ''REFSTARTTIME'' AND
+        referencetime <= ''REFENDTIME'' AND
+        validtimeto >= ''VALIDSTARTTIME'' AND
+        validtimefrom <= ''VALIDENDTIME'' AND
+        dataprovidername=g.child AND
+        g.parent in (DATAPROVIDERSTRING)" 
+    
+    progString<-paste(paste("''",prg,"hours''",collapse=","))
+    query <- sub("PROGSTRING",progString,query)
+    wmoString<-paste(paste("''",wmo_no,"''",sep=""),collapse=",")
+    query <- sub("WMOSTRING",wmoString,query)
+    fstarttime<-getFormattedTime(period=period[1],subtracthours=max(prg))
+    fendtime<-getFormattedTime(period=period[2],addDay=TRUE)
+    query <- sub("REFSTARTTIME",fstarttime,query)
+    query <- sub("REFENDTIME",fendtime,query)
+    validstarttime<-getFormattedTime(period=period[1])
+    validendtime<-getFormattedTime(period=period[2],addDay=TRUE)
+    query <- sub("VALIDSTARTTIME",validstarttime,query)
+    query <- sub("VALIDENDTIME",validendtime,query)
+    dataprovider<-dataproviderDefinitions[dataproviderDefinitions$shortmodelname==mod,]$dataprovider
+    dataproviderstring<-paste("''",dataprovider,"''",sep="")
+    query <- sub("DATAPROVIDERSTRING",dataproviderstring,query)
+    valueparameternames<-vector()
+    for (par in prm){
+      pdef <- parameterDefinitions[parameterDefinitions$miopdb_par==par,]
+      valueparametername <- as.character(pdef$valueparametername)
+      valueparameternames<-c(valueparameternames,valueparametername)
+    }
+    valueparameternames <- sort(valueparameternames)
+    parameterstring2<-paste(paste("''",valueparameternames,"''",sep=""),collapse=",")
+    parameterstring<-paste(paste("\"",valueparameternames,"\" float",sep=""),collapse=" ,")
+    query <- sub("PARAMETERSTRING",parameterstring2,query)
+              
+    
+      if (verbose) {
+          cat(query,"\n") 
+      }
+
+
+    crosstabquery <- "select * from crosstab('SOURCEQUERY','CATEGORY') as ct(ASCT)"
+    sourcequery <- sub("QUERY",query,sourcequery)
+    catquery <- sub("QUERY",query,catquery)     
+    asct_sql <- paste("rowid text,wmo_no text, time text, prog int,",parameterstring)      
+    crosstabquery <- sub("SOURCEQUERY",sourcequery,crosstabquery)
+    crosstabquery <- sub("ASCT",  asct_sql,crosstabquery)
+    crosstabquery <- sub("CATEGORY",  catquery,crosstabquery)
+      
+    
+    if (verbose) {
+      cat(crosstabquery,"\n\n") 
+    }
+
+    
+    rs <- dbSendQuery(con, crosstabquery)
+    results<-fetch(rs,n=-1)
+    dbClearResult(rs)
+
+    # remove first column from results, just used for crosstab
+    results<-results[,-1]
+      
+    mynames <- colnames(results)
+      if (nrow(results)!=0){
+        newresults<-data.frame(matrix(ncol=ncol(results),nrow=nrow(results)))
+        
+        # data values start at FIRSTDATACOLUMN (columns preceeding this one are wmo_no, time and prog).
+        FIRSTDATACOLUMN <- 4
+        
+        for (i in 1:ncol(results)){
+          if (i<FIRSTDATACOLUMN){
+            newresults[,i]=results[,i]
+            colnames(newresults)[i] <-mynames[i] 
+          } else{
+           # find corresponding miopdb parameter name
+            pdef <- parameterDefinitions[parameterDefinitions$valueparametername==mynames[i],]
+            # par is miopdb parameter name
+            par <- as.character(pdef$miopdb_par)
+            parmod <- paste(par,mod,sep=".")
+            #find which columm number to use (same order as requested in input)
+            addcol<-which(par==prm)[1]-1
+            colnumber <- FIRSTDATACOLUMN+addcol
+            colnames(newresults)[colnumber] <-parmod           
+            values<-as.double(unlist(results[,i]))
+            #convert values to other units
+            values <- convertValues(values,par)
+            newresults[,colnumber]=values
+          }
+        }
+      }
+
+      
+      if (nrow(allmodels)==0){      
+        allmodels<-newresults
+      }
+      else{
+        if (nrow(newresults)!=0){      
+          allmodels<-merge(allmodels,newresults,all=T)
+        }
+        
+      }
+
+  }
+      
+  return(allmodels)  
+
+  
+}
+
+
+
+
+
+
 readEnsembleWdb<-function(wmo_no,period,prm,model,prg,lev=NULL,members=NULL){
   # example call
-  # readEnsembleWdb(wmo_no=c(10380,98790),period=c(20120310,20120312),model=c("EPS"),parameters=c("TT"),prg=c(12,18))->mydf
+  # readEnsembleWdb(wmo_no=c(10380,98790),period=c(20120310,20120312),model=c("EPS"),prm=c("TT"),prg=c(12,18))->mydf
   # readEnsembleWdb(wmo_no=c(18700),period=c(20120310,20120312),model=c("EPS"),parameters=c("TCC"),prg=c(12),members=c(0:10))-> mydf
   
   # selection of ref and valid time, same for all queries
@@ -369,8 +501,6 @@ readEnsembleWdb<-function(wmo_no,period,prm,model,prg,lev=NULL,members=NULL){
       rs <- dbSendQuery(con, query)
       results<-fetch(rs,n=-1)
       results<-results[,-1]
-
-
 
       if (nrow(results)!=0){
         for (i in 1:ncol(results)){
